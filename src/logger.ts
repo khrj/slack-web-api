52dc2aa3890fe9a866d12364161704dbdd5412e8
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-02-10 18:02:09 +0530

    Add logger

---
+++ src/logger.ts
@@ -0,0 +1,24 @@
+import { Logger, LogLevel, ConsoleLogger } from 'https://deno.land/x/slack_logger@3.0.0/mod.ts'
+
+let instanceCount = 0
+
+/**
+ * INTERNAL interface for getting or creating a named Logger.
+ */
+export function getLogger(name: string, level: LogLevel, existingLogger?: Logger): Logger {
+    // Get a unique ID for the logger.
+    const instanceId = instanceCount
+    instanceCount += 1
+
+    // Set up the logger.
+    const logger: Logger = (() => {
+        if (existingLogger !== undefined) { return existingLogger }
+        return new ConsoleLogger()
+    })()
+    logger.setName(`web-api:${name}:${instanceId}`)
+    if (level !== undefined) {
+        logger.setLevel(level)
+    }
+
+    return logger
+}
