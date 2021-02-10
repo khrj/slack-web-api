3ad2778451a4f2316ba52499a60eec243ce9fe5c
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-02-10 18:02:01 +0530

    Add helpers

---
+++ src/helpers.ts
@@ -0,0 +1,10 @@
+/**
+ * Build a Promise that will resolve after the specified number of milliseconds.
+ * @param ms milliseconds to wait
+ * @param value value for eventual resolution
+ */
+export function delay(ms: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), ms)
+    })
+}
